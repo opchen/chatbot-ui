5b18fcd62b9505d28a22043b141a443a1d0776e5
Michael <opchen@gmail.com>
2023-05-06 10:27:07 +0800

    fix ai path

--- utils/app/api.ts
+++ utils/app/api.ts
@@ -2,12 +2,12 @@ import { Plugin, PluginID } from '@/types/plugin';
 
 export const getEndpoint = (plugin: Plugin | null) => {
   if (!plugin) {
-    return 'ai/api/chat';
+    return '/ai/api/chat';
   }
 
   if (plugin.id === PluginID.GOOGLE_SEARCH) {
-    return 'ai/api/google';
+    return '/ai/api/google';
   }
 
-  return 'ai/api/chat';
+  return '/ai/api/chat';
 };
